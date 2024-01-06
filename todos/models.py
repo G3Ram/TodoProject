513b921081d8d57e0ab6023f7167b0a872038142
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2024-01-06 12:28:38 -0600

    feat: created todo table by running migrations for todo in postgres

--- todos/models.py
+++ todos/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 # Create your models here.
-# class Todo(models.Model):
-#     content = models.TextField()
+class Todo(models.Model):
+    content = models.TextField()
